4eb79ee9e1c44f59662747f20270945b17e4dbe2
alexlewzey <a.lewzey@hotmail.co.uk>
2021-05-08 09:27:17 +0100

    bug: activate conda env in bash script

--- create_gcp_fastai_env.sh
+++ create_gcp_fastai_env.sh
@@ -3,7 +3,9 @@ conda uninstall jupyterlab -y
 conda install -c conda-forge jupyterlab=3 -y
 # create environment with latestest version of python
 conda create --name fastai python=3.9 -y
+eval "$(conda shell.bash hook)"
 conda activate fastai
+echo $CONDA_PREFIX
 # install dependencies
 conda install -c fastai -c pytorch fastai -y
-conda install -c conda-forge gh
+conda install -c conda-forge gh -y
