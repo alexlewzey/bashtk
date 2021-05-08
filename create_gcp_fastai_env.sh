0ae883e26da3c735d25fbb075255cc5217d6cff9
alexlewzey <a.lewzey@hotmail.co.uk>
2021-05-08 10:02:28 +0100

    bug: finding kernel in jupyter

--- create_gcp_fastai_env.sh
+++ create_gcp_fastai_env.sh
@@ -3,9 +3,10 @@ conda uninstall jupyterlab -y
 conda install -c conda-forge jupyterlab=3 -y
 # create environment with latestest version of python
 conda create --name fastai python=3.9 -y
-eval "$(conda shell.bash hook)"
-conda activate fastai
+# eval "$(conda shell.bash hook)"
+source /opt/conda/bin/activate fastai
 echo $CONDA_PREFIX
-# install dependencies
+# needed to find the kernel in jupyter
+conda install ipykernel -y
 conda install -c fastai -c pytorch fastai -y
 conda install -c conda-forge gh -y
