9863ce584366fb34edba34612cea34c2dac782e2
alexlewzey <a.lewzey@hotmail.co.uk>
2021-09-24 07:39:09 +0100

    feat: add mvcd

--- create_gcp_fastai_env.sh
+++ create_gcp_fastai_env.sh
@@ -1,12 +1,23 @@
 # Remove old jupyterlab and reinstall the latest one
 conda uninstall jupyterlab -y
 conda install -c conda-forge jupyterlab=3 -y
+# allows total level env to see conda envs
+conda install -n notebook_env nb_conda_kernels
 # create environment with latestest version of python
 conda create --name fastai python=3.9 -y
+# allows total level env to see conda envs
+conda install -n notebook_env nb_conda_kernels
 # eval "$(conda shell.bash hook)"
 source /opt/conda/bin/activate fastai
 echo $CONDA_PREFIX
+conda install -c fastai -c pytorch fastai -y
+
 # needed to find the kernel in jupyter
 conda install ipykernel -y
-conda install -c fastai -c pytorch fastai -y
-conda install -c conda-forge gh -y
+
+# make tqdm work in notebook
+conda install -c conda-forge ipywidgets -y
+jupyter nbextension enable --py widgetsnbextension
+
+git clone https://github.com/alexlewzey/slibtk
+pip install -e slibtk/
--- unix_config.sh
+++ unix_config.sh
@@ -9,6 +9,7 @@ alias enc='pycharm /Users/alexlewzey/clmac/clmac/macro/encodings.py'
 alias bp='pycharm /Users/alexlewzey/clmac/clmac/config/boilerplate.py'
 alias br='pycharm /Users/alexlewzey/.bashrc'
 alias cmr='bash ~/clmac/run.sh'
+alias mvcd='source ~/bashtk/mvcd'
 
 # unix tools
 alias sizes='find . -type f -exec du -h {} + | sort -r -h | head'
