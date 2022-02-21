57262f4125ffc8e754ae8733fb916a43ba45f3b4
alexlewzey <a.lewzey@hotmail.co.uk>
2022-02-21 16:29:39 +0000

    feat: python venv convenience alias

--- unix_config.sh
+++ unix_config.sh
@@ -2,7 +2,6 @@
 alias dt='cd ~/Desktop'
 alias dl='cd ~/Downloads'
 alias gd='cd ~/Google\ Drive'
-alias ve='cd ~/.local/share/virtualenvs'
 
 # applications
 alias enc='pycharm /Users/alexlewzey/clmac/clmac/macro/encodings.py'
@@ -10,6 +9,7 @@ alias bp='pycharm /Users/alexlewzey/clmac/clmac/config/boilerplate.py'
 alias br='pycharm /Users/alexlewzey/.bashrc'
 alias cmr='bash ~/clmac/run.sh'
 alias mvcd='source ~/bashtk/mvcd'
+alias venv="if [ -e ./.venv/bin/activate ]; then source ./.venv/bin/activate; else python3 -m venv .venv && source ./.venv/bin/activate; fi"
 
 # unix tools
 alias sizes='find . -type f -exec du -h {} + | sort -r -h | head'
