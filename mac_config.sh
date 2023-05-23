429f4e442c0fd0a4498eb72e685c610c4f5ee3c1
Alexander Lewzey <a.lewzey@gmail.com>
2023-05-23 21:40:12 +0100

    added sql formatter

--- mac_config.sh
+++ mac_config.sh
@@ -3,4 +3,8 @@ alias dt='cd ~/Desktop'
 alias dl='cd ~/Downloads'
 alias gd='cd ~/Google\ Drive/My\ Drive'
 alias ry='cd ~/repository'
-alias ao='cd ~/Google\ Drive/My\ Drive/audio'
+
+ln -s ~/Desktop ~/dt
+ln -s ~/Downloads ~/dl
+ln -s ~/Google\ Drive/My\ Drive ~/gd
+ln -s ~/repository ~/ry
