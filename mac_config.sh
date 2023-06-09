a01543a6f8b29045a2412fc4cb4ca5ecb84faaeb
Alexander Lewzey <a.lewzey@gmail.com>
2023-06-09 21:19:17 +0100

    added git alias

--- mac_config.sh
+++ mac_config.sh
@@ -1,10 +1,5 @@
 # directory shortcuts
-alias dt='cd ~/Desktop'
-alias dl='cd ~/Downloads'
-alias gd='cd ~/Google\ Drive/My\ Drive'
-alias ry='cd ~/repository'
-
-ln -s ~/Desktop ~/dt
-ln -s ~/Downloads ~/dl
-ln -s ~/Google\ Drive/My\ Drive ~/gd
-ln -s ~/repository ~/ry
+ln -sf ~/Desktop/ ~/dt
+ln -sf ~/Downloads/ ~/dl
+ln -sf ~/Google\ Drive/My\ Drive/ ~/gd
+ln -sf ~/repository/ ~/ry
